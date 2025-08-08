4e0996b99b6fc6e7f4152d42e16a809bf78c8f09
voidsky <voidsky02@gmail.com>
2025-08-07 21:34:48 -0700

    finished schema rules for user and clothingItems

--- models/user.js
+++ models/user.js
@@ -2,3 +2,8 @@
 
 /* name — the name of the user, a required string from 2 to 30 characters
     avatar — a required string for the URL of the user's image */
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, minLength: 2, maxLength: 30 },
+  avatar: { type: String, required: true },
+});
