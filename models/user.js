4a700f4141c96f0cbb9347884ba23fc74a2e9bec
voidsky <voidsky02@gmail.com>
2025-09-04 23:33:03 -0700

    running school tests - savepoint

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const { serverErrorHandler } = require("../utils/errors");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true, minLength: 2, maxLength: 30 },
@@ -41,16 +42,17 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(
     .then((user) => {
       if (!user) {
         // return 401 error
-        return Promise.reject(new Error("Incorrect email or password"));
+        return Promise.reject({ message: "Incorrect email or password" });
       }
       return bcrypt.compare(password, user.password).then((matched) => {
         if (!matched) {
           // return 401 error
-          return Promise.reject(new Error("Incorrect email or password"));
+          return Promise.reject({ message: "Incorrect email or password" });
         }
         return user;
       });
-    });
+    })
+    .catch((err) => serverErrorHandler(req, res, err));
 };
 
 const User = mongoose.model("user", userSchema);
